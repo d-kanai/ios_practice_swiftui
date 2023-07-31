ec2d5f8fff2bd3d202af487e963445b27fa045d1
d.kanai <contact@dkanai.com>
2023-08-01 07:16:50 +0900

    save

--- ios_practice_swiftui/ContentView.swift
+++ ios_practice_swiftui/ContentView.swift
@@ -2,7 +2,15 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    @State private var todoTitle = ""
     var body: some View {
+        TextField("create todo", text: $todoTitle)
+        Button(action: {
+            self.todoTitle = ""
+            
+        }){
+            Text("submit")
+        }
         Text("Hello")
     }
 }
