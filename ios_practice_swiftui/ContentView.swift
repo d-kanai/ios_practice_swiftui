4302148cdf551272c85286b857aa754a5029ae1b
d.kanai <contact@dkanai.com>
2023-08-01 07:21:51 +0900

    save

--- ios_practice_swiftui/ContentView.swift
+++ ios_practice_swiftui/ContentView.swift
@@ -5,6 +5,12 @@ struct ContentView: View {
     @State private var todoTitle = ""
     var body: some View {
         TextField("create todo", text: $todoTitle)
+        Button(action: {
+            self.todoTitle = ""
+            
+        }) {
+            Text("submit")
+        }
         Text("Hello")
     }
 }
