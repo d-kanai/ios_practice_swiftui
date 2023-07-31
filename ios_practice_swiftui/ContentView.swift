b28aec190209d3c28b47ff2fd70f0777bad52c31
d.kanai <contact@dkanai.com>
2023-08-01 07:33:21 +0900

    save

--- ios_practice_swiftui/ContentView.swift
+++ ios_practice_swiftui/ContentView.swift
@@ -3,10 +3,11 @@ import CoreData
 
 struct ContentView: View {
     @State private var todoTitle = ""
-    @State private var items = ["clean room"]
+    @State private var items:[String] = []
     var body: some View {
         TextField("create todo", text: $todoTitle)
         Button(action: {
+            self.items.append("\(todoTitle)")
             self.todoTitle = ""
             
         }) {
