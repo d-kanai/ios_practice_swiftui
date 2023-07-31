f8b163f74bdadd85e035c072ea867881a8d292cc
d.kanai <contact@dkanai.com>
2023-07-31 20:08:53 +0900

    todo list

--- ios_practice_swiftui/ContentView.swift
+++ ios_practice_swiftui/ContentView.swift
@@ -2,8 +2,23 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    @State private var todoTitle = ""
+    @State var buttonText = "Button"
+    @State var items:[String] = []
+    
     var body: some View {
-        Text("Hello")
+        TextField("create todo", text: $todoTitle)
+        Button(action: {
+            self.items.append("\(todoTitle)")
+            self.todoTitle = ""
+        }){
+            Text("submit")
+        }
+        List {
+            ForEach($items, id: \.self) { $item in
+                Text("\(item)")
+            }
+        }
     }
 }
 
